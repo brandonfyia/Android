c2960f5f6ce05568408baf41e7c86292d4bee05b
Brandon Sease <brandonfyia@gmail.com>
2013-06-13 01:05:13 -0400

    App now saves and restores state on orientation change. The poster image doesnt show up immidiately though. However If you change the orientation it shows up. Odd.

--- Java2/gen/com/example/java1week2_4/R.java
+++ Java2/gen/com/example/java1week2_4/R.java
@@ -29,12 +29,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f0a000b;
+        public static final int action_settings=0x7f0a000c;
         public static final int criticsTV=0x7f0a000a;
         public static final int gridLayout=0x7f0a0005;
         public static final int header=0x7f0a0001;
         public static final int mainLayout=0x7f0a0000;
         public static final int mpaaTV=0x7f0a0008;
+        public static final int posterIV=0x7f0a000b;
         public static final int runtimeTV=0x7f0a0009;
         public static final int scrollView1=0x7f0a0004;
         public static final int searchButton=0x7f0a0003;
