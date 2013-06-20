9056e9ff81ff6c7e88835f3f00bb8cc11b49e274
Brandon Sease <brandonfyia@gmail.com>
2013-06-20 00:18:16 -0400

    Explicit intent is working and sending data to and recieving data from Movie Picker activity. Data is just test strings at the moment. Will replace with real data next.

--- Java2/gen/com/example/java1week2_4/R.java
+++ Java2/gen/com/example/java1week2_4/R.java
@@ -29,19 +29,21 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f0a000e;
+        public static final int action_settings=0x7f0a0010;
         public static final int criticsTV=0x7f0a000b;
         public static final int gridLayout=0x7f0a0006;
         public static final int header=0x7f0a0002;
+        public static final int headerTV=0x7f0a000d;
         public static final int listTV=0x7f0a0000;
         public static final int mainLayout=0x7f0a0001;
         public static final int mpaaTV=0x7f0a0009;
-        public static final int pickerList=0x7f0a000d;
+        public static final int pickerList=0x7f0a000f;
         public static final int posterIV=0x7f0a000c;
         public static final int runtimeTV=0x7f0a000a;
         public static final int scrollView1=0x7f0a0005;
         public static final int searchButton=0x7f0a0004;
         public static final int searchField=0x7f0a0003;
+        public static final int testButton=0x7f0a000e;
         public static final int titleTV=0x7f0a0007;
         public static final int yearTV=0x7f0a0008;
     }
